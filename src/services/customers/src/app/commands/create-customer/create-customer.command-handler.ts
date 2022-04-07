d66d25e2f28ca57a4c0127aaa3b59b712f6932da
Mikołaj Wargowski <mikoajw20@gmail.com>
2022-04-07 21:58:21 +0200

    ✨ Check is fraudster on customer creation

--- src/services/customers/src/app/commands/create-customer/create-customer.command-handler.ts
+++ src/services/customers/src/app/commands/create-customer/create-customer.command-handler.ts
@@ -1,6 +1,7 @@
 import { Customer } from '@core/customer/customer';
-import { CommandHandler } from '@krater/building-blocks';
+import { AppError, CommandHandler } from '@krater/building-blocks';
 import { CreateCustomerCommand } from './create-customer.command';
+import fetch from 'node-fetch';
 
 export interface CreateCustomerCommandResult {
   id: string;
@@ -13,6 +14,13 @@ export class CreateCustomerCommandHandler
   implements CommandHandler<CreateCustomerCommand, CreateCustomerCommandResult>
 {
   public async handle({ payload }: CreateCustomerCommand): Promise<CreateCustomerCommandResult> {
+    const res = await fetch('http://localhost:4001/api/v1/fraud-check/1');
+    const { isFraudulent } = (await res.json()) as any;
+
+    if (isFraudulent) {
+      throw new AppError(`Customer is fraudster.`);
+    }
+
     const customer = new Customer('1', payload.firstName, payload.lastName, payload.email);
 
     return customer.toJSON();
